c72029c6a0b3a05e5fa56121c2feb97ed7533544
netserx <144199801+netserx@users.noreply.github.com>
2023-09-28 16:23:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let countDownDate = new Date("oct 30, 2023 23:59:59").getTime();
+
+// console.log(countDownDate);
+
+
+let counter = setInterval(() => {
+
+    let dateNow = new Date().getTime();
+
+    let dateDiff = countDownDate - dateNow;
+
+
+
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+
+    document.querySelector(".days").innerHTML = days;
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
+    
+
+    if (dateDiff < 0){
+        clearInterval(counter);
+    }
+}, 1000);
